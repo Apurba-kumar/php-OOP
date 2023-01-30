fcaa5df6839b7fbc07d789d2be8f3648d4bfbf7b
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2023-01-30 23:48:28 +0600

    abstract class

---
+++ abstract_class.php
@@ -0,0 +1,16 @@
+<?php
+abstract class parentclass{
+    public $name;
+    abstract protected function calc($a, $b);
+}
+
+class childclass extends parentclass{
+    public function calc($a,$b){
+        echo $a + $b;
+    }
+}
+$test = new childclass();
+$test->calc(10, 20);
+
+
+?>
